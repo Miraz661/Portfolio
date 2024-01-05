04dfa272b36bc9aeaff284219446b9584d46825d
miraz <105309402+Miraz661@users.noreply.github.com>
2024-01-05 22:20:31 +0600

    Update About.jsx

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -1,10 +1,10 @@
-
+import Project1 from '../../assets/Images/Project1.png'
 
 function About() {
   return (
     <div>
       <h1 className="text-orange-400 text-4xl text-center pb-3 mb-6">About Me</h1>
-      <img src="../../assets/Images/Project1.png"/>
+      <img src={Project1}/>
     </div>
   )
 }
