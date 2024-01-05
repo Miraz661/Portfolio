a2521fa9c2c547fbc0dad514ca4917eca2c18220
miraz <105309402+Miraz661@users.noreply.github.com>
2024-01-05 22:16:22 +0600

    Update About.jsx

--- src/Components/Pages/About.jsx
+++ src/Components/Pages/About.jsx
@@ -4,6 +4,7 @@ function About() {
   return (
     <div>
       <h1 className="text-orange-400 text-4xl text-center pb-3 mb-6">About Me</h1>
+      <img src="../../assets/Images/uu-logo.png"/>
     </div>
   )
 }
