cb31ba59c0cfcd18f5272d7e2c7fbb8bfb960a10
miraz <105309402+Miraz661@users.noreply.github.com>
2024-01-05 22:02:03 +0600

    Update Education.jsx

--- src/Components/Pages/Education.jsx
+++ src/Components/Pages/Education.jsx
@@ -6,13 +6,13 @@ function Education() {
       <h1 className="text-orange-400 text-center text-4xl mt-6 mb-8">Educational Background</h1>
       <div className="flex flex-wrap w-full border-b-4 border-orange-400 pb-3 mb-6">
         <div>
-          <img src='/assets/Images/Project1.png'/>
+          <img src='../../assets/Images/uu-logo.png'/>
         </div>
         <div className="py-4">
           <h1 className="text-4xl">Uttara University</h1>
           <h2 className="text-2xl">Bachelor of Computer Science and Engineering</h2>
           <h3 className="text-xl">January 2021 - running</h3>
-          
+          <h3 className="text-xl">Dhaka,Bangladesh</h3>
         </div>
       </div>
     </div>
