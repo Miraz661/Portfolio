41c33df5ea9211aa5c9bd1c2db6213282695a248
Miraz Hossain <mirazh661@gmail.com>
2025-05-05 15:38:04 +0600

    Skills added.

--- src/Components/Pages/Skills.jsx
+++ src/Components/Pages/Skills.jsx
@@ -1,112 +1,115 @@
-import {FaCss3, FaHtml5, FaJs, FaReact} from 'react-icons/fa'
-import {SiC, SiCplusplus, SiFirebase, SiMysql, SiPython, SiTailwindcss} from 'react-icons/si'
-import {TbBrandVscode} from 'react-icons/tb'
-import {BsGit,BsGithub} from 'react-icons/bs'
-import {BiLogoNetlify} from 'react-icons/bi'
-import { FaChartBar } from "react-icons/fa"
-import { PiMicrosoftExcelLogoFill } from "react-icons/pi"
+import { FaCss3, FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import {
+  SiC,
+  SiCplusplus,
+  SiFirebase,
+  SiMysql,
+  SiPython,
+  SiTailwindcss,
+} from "react-icons/si";
+import { TbBrandVscode } from "react-icons/tb";
+import { BsGit, BsGithub } from "react-icons/bs";
+import { BiLogoNetlify } from "react-icons/bi";
+import { FaChartBar } from "react-icons/fa";
+import { PiMicrosoftExcelLogoFill } from "react-icons/pi";
 
 function Skills() {
   return (
     <div>
-      <h1 className="text-center text-[orange] text-3xl pt-2 pb-8">My Skills</h1>
+      <h1 className="text-center text-[orange] text-3xl pt-2 pb-8">
+        My Skills
+      </h1>
       <div className="w-full flex justify-center text-neutral-400">
-        <div>
-          <div className="flex justify-between">
-            <div>
-              <h1 className='text-center text-2xl pb-2 text-white'>Languages</h1>
-              <ul className="flex justify-between">
+        <div className="flex flex-wrap md:block">
+          <div className="flex justify-between flex-wrap gap-5">
+            <div className="flex-1 min-w-[350px]">
+              <h1 className="text-center text-2xl pb-2 text-white">
+                Languages
+              </h1>
+              <ul className="flex justify-evenly">
                 <div>
-                  <li className='flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer'>
-                    <FaJs className='text-2xl pt-0.5 pr-1 text-yellow-300'/>
+                  <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
+                    <FaJs className="text-2xl pt-0.5 pr-1 text-yellow-300" />
                     <span>JavaScript</span>
                   </li>
-                  <li className='flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer'>
-                    <SiPython className='text-2xl pt-0.5 pr-1 text-yellow-300'/>
+                  <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
+                    <SiPython className="text-2xl pt-0.5 pr-1 text-yellow-300" />
                     <span>Python</span>
                   </li>
                 </div>
                 <div>
-                  <li className='flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer'>
-                    <SiC className='text-2xl pt-0.5 pr-1 text-blue-700'/>
+                  <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
+                    <SiC className="text-2xl pt-0.5 pr-1 text-blue-700" />
                     <span>C</span>
                   </li>
-                  <li className='flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer'>
-                    <SiCplusplus className='text-2xl pt-0.5 pr-1 text-blue-800'/>
+                  <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
+                    <SiCplusplus className="text-2xl pt-0.5 pr-1 text-blue-800" />
                     <span>C++</span>
                   </li>
                 </div>
               </ul>
             </div>
-            <div>
-              <h1 className='text-center text-2xl pb-2 text-white'>FrontEnd</h1>
-              <ul className="flex justify-between">
+            <div className="flex-1 min-w-[350px]">
+              <h1 className="text-center text-2xl pb-2 text-white">FrontEnd</h1>
+              <ul className="flex justify-evenly">
                 <div>
                   <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                    <FaHtml5 className='text-2xl pt-0.5 pr-1 text-orange-600'/>
+                    <FaHtml5 className="text-2xl pt-0.5 pr-1 text-orange-600" />
                     <span>HTML5</span>
                   </li>
                   <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                    <FaCss3 className='text-2xl pt-0.5 pr-1 text-blue-700'/>
+                    <FaCss3 className="text-2xl pt-0.5 pr-1 text-blue-700" />
                     <span>CSS3</span>
                   </li>
                 </div>
                 <div>
                   <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                    <FaReact className='text-2xl pt-0.5 pr-1 text-cyan-600'/>
+                    <FaReact className="text-2xl pt-0.5 pr-1 text-cyan-600" />
                     <span>ReactJS</span>
                   </li>
                   <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                    <SiTailwindcss className='text-2xl pt-0.5 pr-1 text-cyan-600'/>
+                    <SiTailwindcss className="text-2xl pt-0.5 pr-1 text-cyan-600" />
                     <span>TailwindCSS</span>
                   </li>
                 </div>
               </ul>
             </div>
           </div>
-          <div className="flex justify-between pt-2">
-            <div>
-              <h1 className='text-center text-2xl pb-2 text-white'>BackEnd</h1>
-              <ul className="flex justify-between">
+          <div className="flex justify-center md:justify-between pt-2 gap-5 flex-wrap max-w-[725px]">
+            <div className="md:w-[350px] w-full">
+              <h1 className="text-center text-2xl pb-2 text-white">BackEnd</h1>
+              <ul className="w-full flex justify-evenly">
                 <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                  <SiFirebase className='text-2xl pt-0.5 pr-1 text-yellow-500'/>
+                  <SiFirebase className="text-2xl pt-0.5 pr-1 text-yellow-500" />
                   <span>Firebase</span>
                 </li>
                 <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                  <SiMysql className='text-2xl pt-0.5 pr-1 text-yellow-600'/>
+                  <SiMysql className="text-2xl pt-0.5 pr-1 text-yellow-600" />
                   <span>MySQL</span>
                 </li>
               </ul>
             </div>
-            <div>
-              <h1 className='text-center text-2xl pb-2 text-white'>Others</h1>
-              <ul className="flex justify-between">
-                <div>
+            <div className="md:w-[350px] w-full">
+              <h1 className="text-center text-2xl pb-2 text-white">Others</h1>
+              <ul className="flex justify-evenly">
+                <div className="">
                   <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                    <TbBrandVscode className='text-2xl pt-0.5 pr-1 text-blue-600'/>
+                    <TbBrandVscode className="text-2xl pt-0.5 pr-1 text-blue-600" />
                     <span>VScode</span>
                   </li>
-                  <li className='flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer'>
-                    <BsGithub className='text-2xl pt-0.5 pr-1 text-black'/>
+                  <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
+                    <BsGithub className="text-2xl pt-0.5 pr-1 text-black" />
                     <span>Github</span>
                   </li>
-                  <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                    <PiMicrosoftExcelLogoFill className='text-2xl pt-0.5 pr-1 text-orange-600'/>
-                    <span>MS Office</span>
-                  </li>
                 </div>
-                <div>
+                <div className="">
                   <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                    <BsGit className='text-2xl pt-0.5 pr-1 text-orange-600'/>
+                    <BsGit className="text-2xl pt-0.5 pr-1 text-orange-600" />
                     <span>Git</span>
                   </li>
                   <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                    <BiLogoNetlify className='text-2xl pt-0.5 pr-1 text-cyan-600'/>
+                    <BiLogoNetlify className="text-2xl pt-0.5 pr-1 text-cyan-600" />
                     <span>Netlify</span>
-                  </li>
-                  <li className="flex px-4 py-4 border-2 border-[orange] rounded-lg m-2 w-[160px] font-semibold cursor-pointer">
-                    <FaChartBar className='text-2xl pt-0.5 pr-1 text-cyan-600'/>
-                    <span>Power BI</span>
                   </li>
                 </div>
               </ul>
@@ -115,7 +118,7 @@ function Skills() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
