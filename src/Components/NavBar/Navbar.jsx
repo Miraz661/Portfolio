be5e7597dfc2d363f77906bf32cc63970e6caf6b
Miraz Hossain <mirazh661@gmail.com>
2025-05-05 15:20:49 +0600

    NavBar overflow bug fixed.

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,54 +1,75 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom";
 
 function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <>
-            <section className="px-8 bg-[#0a192f] text-white fixed w-full">
-                <nav className='grid grid-cols-12 py-4'>
-                    <h1 className="col-span-5 font-semibold text-2xl">
-                        <Link to='/'>
-                            <span>爪丨尺卂乙</span>
-                        </Link>
-                    </h1>
-                    <div className={`col-span-7 flex ${isOpen ? 'justify-between' : 'justify-end'} md:block`}>
-                        <ul onClick={() => setIsOpen(false)} className={`font-semibold,pt-10 md:pt-0 text-center text-lg md:grid-cols-6 ${isOpen ? 'md:grid' : 'hidden'} md:grid`}>
-                            <Link to="/">
-                                <li className="hover:text-orange-400 font-semibold">Home</li>
-                            </Link>
-                            <Link to="/education">
-                                <li className="hover:text-orange-400 font-semibold">Education</li>
-                            </Link>
-                            <Link to="/skills">
-                                <li className="hover:text-orange-400 font-semibold">Skills</li>
-                            </Link>
-                            <Link to="/projects">
-                                <li className="hover:text-orange-400 font-semibold">Projects</li>
-                            </Link>
-                            <Link to="/research">
-                                <li className="hover:text-orange-400 font-semibold">Research</li>
-                            </Link>
-                            <Link to="/about">
-                                <li className="hover:text-orange-400 font-semibold">About Me</li>
-                            </Link>
-
-                        </ul>
-                        {
-                            isOpen ?
-                                <FaTimes onClick={() => setIsOpen(!isOpen)} className="text-2xl block md:hidden" />
-                                :
-                                <FaBars onClick={() => setIsOpen(!isOpen)} className="text-2xl block md:hidden" />
-                        }
-                    </div>
-                </nav>
-            </section>
-            <div className='pt-20 mx-8 text-white'>
-                <Outlet></Outlet>
-            </div>
-        </>
-    )
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <section className="px-8 bg-[#0a192f] text-white fixed w-full z-[999]">
+        <nav className="grid grid-cols-12 py-4">
+          <h1 className="col-span-5 font-semibold text-2xl">
+            <Link to="/">
+              <span>爪丨尺卂乙</span>
+            </Link>
+          </h1>
+          <div
+            className={`col-span-7 flex ${
+              isOpen ? "justify-between" : "justify-end"
+            } md:block`}
+          >
+            <ul
+              onClick={() => setIsOpen(false)}
+              className={`font-semibold,pt-10 md:pt-0 text-center text-lg md:grid-cols-6 ${
+                isOpen ? "md:grid" : "hidden"
+              } md:grid`}
+            >
+              <Link to="/">
+                <li className="hover:text-orange-400 font-semibold">Home</li>
+              </Link>
+              <Link to="/education">
+                <li className="hover:text-orange-400 font-semibold">
+                  Education
+                </li>
+              </Link>
+              <Link to="/skills">
+                <li className="hover:text-orange-400 font-semibold">Skills</li>
+              </Link>
+              <Link to="/projects">
+                <li className="hover:text-orange-400 font-semibold">
+                  Projects
+                </li>
+              </Link>
+              <Link to="/research">
+                <li className="hover:text-orange-400 font-semibold">
+                  Research
+                </li>
+              </Link>
+              <Link to="/about">
+                <li className="hover:text-orange-400 font-semibold">
+                  About Me
+                </li>
+              </Link>
+            </ul>
+            {isOpen ? (
+              <FaTimes
+                onClick={() => setIsOpen(!isOpen)}
+                className="text-2xl block md:hidden"
+              />
+            ) : (
+              <FaBars
+                onClick={() => setIsOpen(!isOpen)}
+                className="text-2xl block md:hidden"
+              />
+            )}
+          </div>
+        </nav>
+      </section>
+      <div className="pt-20 mx-8 text-white">
+        <Outlet></Outlet>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
--- src/Components/Pages/Projects.jsx
+++ src/Components/Pages/Projects.jsx
@@ -2,6 +2,7 @@ import { BiLinkExternal } from "react-icons/bi";
 import { FiGithub } from "react-icons/fi";
 import Project1 from "../../assets/Images/Project1.png";
 import Project2 from "../../assets/Images/Project2.png";
+import Project3 from "../../assets/Images/Project3.png";
 
 function Projects() {
   return (
@@ -9,131 +10,119 @@ function Projects() {
       <h1 className="text-orange-400 text-4xl text-center pb-3 mb-6">
         My Projects
       </h1>
-      <div className="flex justify-center flex-wrap">
-        <div className="max-w-sm  overflow-hidden p-4 shadow-2xl hover:-translate-y-2 duration-300">
+      <div className="flex justify-center flex-wrap gap-5">
+        <div className="max-w-sm min-w-[350px] overflow-hidden p-4 shadow-2xl hover:-translate-y-2 duration-300">
           <img
-            className="w-full rounded-xl"
+            className="w-full rounded-xl h-[180px]"
             src={Project1}
             alt="Sunset in the mountains"
           />
           <div className="px-4 pt-2 pb-2">
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              ReactJs
+              ReactJS
             </span>
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              Tailwind
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              ExpressJs
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              MySQl
+              TailwindCSS
             </span>
           </div>
 
           <div className="px-6 py-0">
             <div className="font-bold text-orange-500 text-xl mb-2">
-              E-Attendance
+              Service Dashboard
             </div>
             <div className="text-white text-base">
               <p className="text-lg text-neutral-400 font-medium mb-4">
                 {" "}
-                A Attendance website to manage daily attendance of students.
+                A Dashboard website showcases detailed service offerings, team
+                profiles,projects, and client engagement.
               </p>
               <ul className="list-disc">
-                <li>
-                  Teachers can easily take attendance using a phone or PC.
-                </li>
-                <li>Search filters and pagination have been applied.</li>
-                <li>Login is required to perform all tasks.</li>
-                <li>New batches or students can be added at any time.</li>
-                <li>Analyzing attendance data can be done in an instant.</li>
+                <li>Clients can view our previous services over the years.</li>
+                <li>What services do we offer?</li>
+                <li>Who are our clients?</li>
               </ul>
             </div>
             <div className="mt-2 flex gap-x-12">
-              <span>
+              <a
+                href="https://e-attendance-system.netlify.app/"
+                target="_blank"
+              >
                 <BiLinkExternal
                   className="text-white text-xl hover:text-orange-500"
                   alt="Live"
                 />
-              </span>
-              <span>
+              </a>
+              <a href="https://github.com/Miraz661/Attendance-System">
                 <FiGithub
                   className="text-white text-xl hover:text-orange-500"
                   alt="Live"
                 />
-              </span>
+              </a>
             </div>
           </div>
         </div>
         <div className="max-w-sm  overflow-hidden p-4 shadow-2xl hover:-translate-y-2 duration-300">
           <img
-            className="w-full rounded-xl"
-            src={Project1}
-            alt="Sunset in the mountains"
-          />
-          <div className="px-4 pt-2 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              ReactJs
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              Tailwind
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              ExpressJs
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              MySQl
-            </span>
-          </div>
-
-          <div className="px-6 py-0">
-            <div className="font-bold text-orange-500 text-xl mb-2">
-              E-Attendance
-            </div>
-            <div className="text-white text-base">
-              <p className="text-lg text-neutral-400 font-medium mb-4">
-                {" "}
-                A Attendance website to manage daily attendance of students.
-              </p>
-              <ul className="list-disc">
-                <li>
-                  Teachers can easily take attendance using a phone or PC.
-                </li>
-                <li>Search filters and pagination have been applied.</li>
-                <li>Login is required to perform all tasks.</li>
-                <li>New batches or students can be added at any time.</li>
-                <li>Analyzing attendance data can be done in an instant.</li>
-              </ul>
-            </div>
-            <div className="mt-2 flex gap-x-12">
-              <span>
-                <BiLinkExternal
-                  className="text-white text-xl hover:text-orange-500"
-                  alt="Live"
-                />
-              </span>
-              <span>
-                <FiGithub
-                  className="text-white text-xl hover:text-orange-500"
-                  alt="Live"
-                />
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="max-w-sm  overflow-hidden p-4 shadow-2xl hover:-translate-y-2 duration-300">
-          <img
-            className="w-full rounded-xl"
+            className="w-full rounded-xl h-[180px]"
             src={Project2}
             alt="Sunset in the mountains"
           />
           <div className="px-4 pt-2 pb-2">
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              ReactJs
+              ReactJS
             </span>
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
-              Tailwind
+              TailwindCSS
+            </span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
+              ExpressJS
+            </span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
+              MySQl
+            </span>
+          </div>
+
+          <div className="px-6 py-0">
+            <div className="font-bold text-orange-500 text-xl mb-2">
+              E-Attendance
+            </div>
+            <div className="text-white text-base">
+              <p className="text-lg text-neutral-400 font-medium mb-4">
+                {" "}
+                A Attendance website to manage daily attendance of students.
+              </p>
+              <ul className="list-disc">
+                <li>
+                  Teachers can easily take attendance using a phone or PC.
+                </li>
+                <li>Search filters and pagination have been applied.</li>
+                <li>Login is required to perform all tasks.</li>
+                <li>New batches or students can be added at any time.</li>
+                <li>Analyzing attendance data can be done in an instant.</li>
+              </ul>
+            </div>
+            <div className="mt-2 flex gap-x-12">
+              <a href="https://github.com/Miraz661/MyDashboard">
+                <FiGithub
+                  className="text-white text-xl hover:text-orange-500"
+                  alt="Live"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="max-w-sm  overflow-hidden p-4 shadow-2xl hover:-translate-y-2 duration-300">
+          <img
+            className="w-full rounded-xl h-[180px]"
+            src={Project3}
+            alt="Sunset in the mountains"
+          />
+          <div className="px-4 pt-2 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
+              ReactJS
+            </span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
+              TailwindCSS
             </span>
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-1 mb-2">
               Daisyui
